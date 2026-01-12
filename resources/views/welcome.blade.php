5a9dec479fb8e2c0264e659c2017ba05e34d54ff
josemachava <jose.machava@xindiri.com>
2026-01-12 19:55:25 +0200

    Change DB User display from paragraph to header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
     </head>
 
 
-    <p>DB User: {{ config('database.connections.mysql.username') }}</p>
+    <h1>DB User: {{ config('database.connections.mysql.username') }}</h1>
 
     <body class="antialiased">
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
